972f9f71ac08a283d94e8ddb3b71745aefb0c6a5
Minh Tran <mtgred@gmail.com>
2011-07-25 05:39:30 +0200

    Added datafetch from server and local storage

--- public/js/pos.js
+++ public/js/pos.js
@@ -1,4 +1,43 @@
 (function() {
+  var Pos, Store, db;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  db = openerp.init();
+  Store = (function() {
+    function Store() {
+      var store;
+      store = localStorage['pos'];
+      this.data = (store && JSON.parse(store)) || {};
+    }
+    Store.prototype.get = function(key) {
+      return this.data[key];
+    };
+    Store.prototype.set = function(key, value) {
+      this.data[key] = value;
+      return localStorage['pos'] = JSON.stringify(this.data);
+    };
+    return Store;
+  })();
+  Pos = (function() {
+    function Pos() {
+      this.session.session_login('pos', 'admin', 'admin', __bind(function() {
+        this.fetch('pos.category', ['name', 'child_id']);
+        return this.fetch('product.product', ['name', 'list_price', 'pos_categ_id', 'taxes_id', 'img']);
+      }, this));
+    }
+    Pos.prototype.session = new db.base.Session('DEBUG');
+    Pos.prototype.store = new Store;
+    Pos.prototype.fetch = function(osvModel, fields, domain) {
+      return this.session.rpc('/base/dataset/search_read', {
+        model: osvModel,
+        fields: fields,
+        domain: domain
+      }, __bind(function(result) {
+        return this.store.set(osvModel, result['records']);
+      }, this));
+    };
+    return Pos;
+  })();
+  openerp.point_of_sale = new Pos;
   $(function() {
     $('#steps').buttonset();
     $('#rightpane').width($(window).width() - 445);
--- src/pos.coffee
+++ src/pos.coffee
@@ -1,7 +1,29 @@
-$ ->
-  # jQuery UI
-  $('#steps').buttonset()
+db = openerp.init()
 
-  # Rightpane resize
+class Store
+  constructor: ->
+    store = localStorage['pos']
+    @data = (store && JSON.parse(store)) || {}
+  get: (key) -> @data[key]
+  set: (key, value) ->
+    @data[key] = value
+    localStorage['pos'] = JSON.stringify(@data)
+
+class Pos
+  constructor: ->
+    @session.session_login 'pos', 'admin', 'admin', =>
+      @fetch 'pos.category', ['name', 'child_id']
+      @fetch 'product.product', ['name', 'list_price', 'pos_categ_id', 'taxes_id', 'img']
+  session: new db.base.Session('DEBUG')
+  store: new Store
+  fetch: (osvModel, fields, domain) ->
+    @session.rpc '/base/dataset/search_read',
+      model: osvModel, fields: fields, domain: domain, (result) =>
+        @store.set osvModel, result['records']
+
+openerp.point_of_sale = new Pos
+
+$ ->
+  $('#steps').buttonset() # jQuery UI button set
   $('#rightpane').width($(window).width() - 445)
   $(window).resize -> $('#rightpane').width($(window).width() - 445)
